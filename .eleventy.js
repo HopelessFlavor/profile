06fc6b7ca4fc6093af55cd487641746771bbaba7
Stanislav Moiseev <72256017+HopelessFlavor@users.noreply.github.com>
2021-09-26 12:21:52 +0300

    Add njk templates

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = (config) => {
+    return {
+        dir: {
+          input: 'src',
+          output: 'dist',
+          includes: 'includes',
+          layouts: 'layouts'
+        },
+        dataTemplateEngine: 'njk',
+        markdownTemplateEngine: 'njk',
+        htmlTemplateEngine: 'njk',
+        passthroughFileCopy: true,
+        templateFormats: [
+          'md', 'njk'
+        ],
+    }
+};
+
